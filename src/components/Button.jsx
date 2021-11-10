3f5e80d1c43c97f05968595afa94bdff1d707704
monica <mnabais@advicefront.com>
2021-11-10 20:23:32 +0000

    Project cleanup

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,10 @@ const isOperator = val => {
   return !isNaN(val) || val === "." || val === "=";
 }
 
-export const Button = props => (
+const Button = props => (
   <div onClick={() => props.handleClick(props.children)} className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}>
     {props.children}
   </div>
 )
+
+export default Button;
